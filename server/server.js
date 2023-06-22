8a6a5a9d53e51d9094ad3d68de941cd8518bf897
shabinasiddiqui <siddiquishabina10@gmail.com>
2023-06-22 13:05:44 +0530

    "change contact.jsx"

--- server/server.js
+++ server/server.js
@@ -1,76 +1,88 @@
+// const express = require("express");
+// const cors = require("cors");
+// const mysql = require("mysql");
+
+// const app = express();
+// app.use(express.json());
+// app.use(cors());
+// const port = 3001;
+
+// const pool = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "root",
+//   database: "contactDetails",
+// });
+
+// // Use the database
+// pool.query(`USE ${pool.config.connectionConfig.database}`, (error) => {
+//   if (error) {
+//     console.error("Error selecting database:", error);
+//   } else {
+//     console.log("Using database:", pool.config.connectionConfig.database);
+//     createContactsTable(); // Create the contacts table after selecting the database
+//   }
+// });
+
+// // Function to create the contacts table
+// function createContactsTable() {
+//   const createTableQuery = `
+//     CREATE TABLE IF NOT EXISTS contacts (
+//       id INT AUTO_INCREMENT PRIMARY KEY,
+//       email VARCHAR(255) NOT NULL,
+//       emailSubject VARCHAR(255) NOT NULL,
+//       fullName VARCHAR(255) NOT NULL,
+//       message TEXT NOT NULL,
+//       mobileNumber VARCHAR(20) NOT NULL
+//     )
+//   `;
+
+//   // Create the contacts table if it doesn't exist
+//   pool.query(createTableQuery, (error) => {
+//     if (error) {
+//       console.error("Error creating contacts table:", error);
+//     } else {
+//       console.log("Contacts table created successfully");
+//     }
+//   });
+// }
+
+// // ... Rest of the server code ...
+
+// app.listen(port, () => {
+//   console.log(`Server is running on http://localhost:${port}`);
+// });
+
+// app.post("/api/contact", (req, res) => {
+//   const { email, emailSubject, fullName, message, mobileNumber } = req.body;
+//   const contactData = {
+//     email,
+//     emailSubject,
+//     fullName,
+//     message,
+//     mobileNumber,
+//   };
+
+//   // Insert the contact details into the database
+//   pool.query("INSERT INTO contacts SET ?", contactData, (error, results) => {
+//     if (error) {
+//       console.error("Error inserting contact details:", error);
+//       res.status(500).json({ success: false }); // Send an error response to the client
+//     } else {
+//       console.log("Contact details inserted successfully");
+//       res.status(200).json({ success: true }); // Send a success response to the client
+//     }
+//   });
+// });
+
 const express = require("express");
 const cors = require("cors");
-const mysql = require("mysql");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 const port = 3001;
 
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "contactDetails",
-});
-
-// Use the database
-pool.query(`USE ${pool.config.connectionConfig.database}`, (error) => {
-  if (error) {
-    console.error("Error selecting database:", error);
-  } else {
-    console.log("Using database:", pool.config.connectionConfig.database);
-    createContactsTable(); // Create the contacts table after selecting the database
-  }
-});
-
-// Function to create the contacts table
-function createContactsTable() {
-  const createTableQuery = `
-    CREATE TABLE IF NOT EXISTS contacts (
-      id INT AUTO_INCREMENT PRIMARY KEY,
-      email VARCHAR(255) NOT NULL,
-      emailSubject VARCHAR(255) NOT NULL,
-      fullName VARCHAR(255) NOT NULL,
-      message TEXT NOT NULL,
-      mobileNumber VARCHAR(20) NOT NULL
-    )
-  `;
-
-  // Create the contacts table if it doesn't exist
-  pool.query(createTableQuery, (error) => {
-    if (error) {
-      console.error("Error creating contacts table:", error);
-    } else {
-      console.log("Contacts table created successfully");
-    }
-  });
-}
-
-// ... Rest of the server code ...
-
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
-});
-
-app.post("/api/contact", (req, res) => {
-  const { email, emailSubject, fullName, message, mobileNumber } = req.body;
-  const contactData = {
-    email,
-    emailSubject,
-    fullName,
-    message,
-    mobileNumber,
-  };
-
-  // Insert the contact details into the database
-  pool.query("INSERT INTO contacts SET ?", contactData, (error, results) => {
-    if (error) {
-      console.error("Error inserting contact details:", error);
-      res.status(500).json({ success: false }); // Send an error response to the client
-    } else {
-      console.log("Contact details inserted successfully");
-      res.status(200).json({ success: true }); // Send a success response to the client
-    }
-  });
 });
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,91 +1,152 @@
-import React, { useState } from "react";
-import axios from "axios";
+// import React, { useState } from "react";
+// import axios from "axios";
+// import "./Contact.css";
+
+// function Contact() {
+//   const [fullName, setFullName] = useState("");
+//   const [email, setEmail] = useState("");
+//   const [mobileNumber, setMobileNumber] = useState("");
+//   const [emailSubject, setEmailSubject] = useState("");
+//   const [message, setMessage] = useState("");
+//   const [isSubmitting, setIsSubmitting] = useState(false);
+//   // const [submitMessage, setSubmitMessage] = useState("");
+//   // const [insertedContact, setInsertedContact] = useState(null);
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+
+//     setIsSubmitting(true);
+
+//     axios
+//       .post("http://localhost:3001/api/contact", {
+//         fullName,
+//         email,
+//         mobileNumber,
+//         emailSubject,
+//         message,
+//       })
+//       .then((response) => {
+//         // setSubmitMessage(response.data.message);
+//         setIsSubmitting(false);
+//       })
+//       .catch((error) => {
+//         console.error(error);
+//         // setSubmitMessage("An error occurred while submitting the form.");
+//         setIsSubmitting(false);
+//       });
+//   };
+
+//   return (
+//     <>
+//       <section className="contact" id="contact">
+//         <h2 className="heading">
+//           Reach out to <span>Me!</span>
+//         </h2>
+//         <form onSubmit={handleSubmit}>
+//           <div className="input-box">
+//             <input
+//               type="text"
+//               placeholder="Full Name"
+//               value={fullName}
+//               onChange={(e) => setFullName(e.target.value)}
+//             />
+//             <input
+//               type="email"
+//               placeholder="Email Address"
+//               value={email}
+//               onChange={(e) => setEmail(e.target.value)}
+//             />
+//           </div>
+//           <div className="input-box">
+//             <input
+//               type="number"
+//               placeholder="Mobile Number"
+//               value={mobileNumber}
+//               onChange={(e) => setMobileNumber(e.target.value)}
+//             />
+//             <input
+//               type="text"
+//               placeholder="Email Subject"
+//               value={emailSubject}
+//               onChange={(e) => setEmailSubject(e.target.value)}
+//             />
+//           </div>
+//           <textarea
+//             name=""
+//             id=""
+//             cols="30"
+//             rows="10"
+//             placeholder="Your Message"
+//             value={message}
+//             onChange={(e) => setMessage(e.target.value)}
+//           ></textarea>
+//           <input
+//             type="submit"
+//             value="Send Message"
+//             className="btn"
+//             disabled={isSubmitting}
+//           />
+//         </form>
+//       </section>
+//     </>
+//   );
+// }
+
+// export default Contact;
+
+import React from "react";
 import "./Contact.css";
 
 function Contact() {
-  const [fullName, setFullName] = useState("");
-  const [email, setEmail] = useState("");
-  const [mobileNumber, setMobileNumber] = useState("");
-  const [emailSubject, setEmailSubject] = useState("");
-  const [message, setMessage] = useState("");
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  // const [submitMessage, setSubmitMessage] = useState("");
-  // const [insertedContact, setInsertedContact] = useState(null);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    setIsSubmitting(true);
-
-    axios
-      .post("http://localhost:3001/api/contact", {
-        fullName,
-        email,
-        mobileNumber,
-        emailSubject,
-        message,
-      })
-      .then((response) => {
-        // setSubmitMessage(response.data.message);
-        setIsSubmitting(false);
-      })
-      .catch((error) => {
-        console.error(error);
-        // setSubmitMessage("An error occurred while submitting the form.");
-        setIsSubmitting(false);
-      });
-  };
-
   return (
     <>
       <section className="contact" id="contact">
         <h2 className="heading">
           Reach out to <span>Me!</span>
         </h2>
-        <form onSubmit={handleSubmit}>
+        <form
+          name="contact"
+          method="POST"
+          data-netlify="true"
+          netlify-honeypot="bot-field"
+        >
+          <input type="hidden" name="form-name" value="contact" />
           <div className="input-box">
             <input
               type="text"
+              name="fullName"
               placeholder="Full Name"
-              value={fullName}
-              onChange={(e) => setFullName(e.target.value)}
+              required
             />
             <input
               type="email"
+              name="email"
               placeholder="Email Address"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              required
             />
           </div>
           <div className="input-box">
             <input
               type="number"
+              name="mobileNumber"
               placeholder="Mobile Number"
-              value={mobileNumber}
-              onChange={(e) => setMobileNumber(e.target.value)}
+              required
             />
             <input
               type="text"
+              name="emailSubject"
               placeholder="Email Subject"
-              value={emailSubject}
-              onChange={(e) => setEmailSubject(e.target.value)}
+              required
             />
           </div>
           <textarea
-            name=""
-            id=""
+            name="message"
             cols="30"
             rows="10"
             placeholder="Your Message"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
+            required
           ></textarea>
-          <input
-            type="submit"
-            value="Send Message"
-            className="btn"
-            disabled={isSubmitting}
-          />
+          <input type="submit" value="Send Message" className="btn" />
         </form>
       </section>
     </>
